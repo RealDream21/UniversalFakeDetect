bd31026f35ef57a9de3f3b69665ce3890231f533
RealDream21 <fabianflorete@yahoo.com>
2025-03-31 17:33:57 +0300

    [FIX] Fix old path
    this path does not exist

--- options/base_options.py
+++ options/base_options.py
@@ -10,7 +10,7 @@ class BaseOptions():
 
     def initialize(self, parser):
         parser.add_argument('--mode', default='binary')
-        parser.add_argument('--arch', type=str, default='res50', help='see my_models/__init__.py')
+        parser.add_argument('--arch', type=str, default='res50', help='see models/__init__.py')
         parser.add_argument('--fix_backbone', action='store_true')  
 
         # data augmentation
